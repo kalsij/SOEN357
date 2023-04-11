43722443db6bf999d255af3f02a6e4ed68e3b0bb
kalsij <63124857+kalsij@users.noreply.github.com>
2023-04-10 20:28:21 -0400

    sign in

---
+++ dresserly/src/Protection.js
@@ -0,0 +1,13 @@
+import { useNavigate,  } from "react-router-dom";
+import { useUserAuth } from "./UserAuthContext";
+
+function Protection({ children}) {
+ 
+  const {user} = useUserAuth();
+  const navigate = useNavigate();
+
+  return (user) ? children : navigate('/');
+};
+
+
+export default Protection;
