6d94ed13b82441fe069a3acdf4dce38d358dd47f
AfandiM <84986555+AfandiM@users.noreply.github.com>
2023-04-18 02:10:07 -0400

    fetched tags and clothes from db to display them on page

--- dresserly/src/Matching/MatchingPage.js
+++ dresserly/src/Matching/MatchingPage.js
@@ -1,55 +1,105 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import "./MatchingPage.css";
-import { Grid } from "@mui/material";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+import { collection, query, getDocs, doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import CloseIcon from "@mui/icons-material/Close";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackwardIosIcon from "@mui/icons-material/ArrowBackIos";
 
 export default function MatchingPage() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const imagesTops = [
-    {
-      url: "https://example.com/image1.jpg",
-      caption: "Image 1",
-    },
-    {
-      url: "https://example.com/image2.jpg",
-      caption: "Image 2",
-    },
-    {
-      url: "https://example.com/image3.jpg",
-      caption: "Image 3",
-    },
-  ];
+  const [currentTopIndex, setCurrentTopIndex] = useState(0);
+  const [currentBottomIndex, setCurrentBottomIndex] = useState(0);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [tags, setTags] = useState([]);
+  const [topClothes, setTopClothes] = useState([]);
+  const [bottomClothes, setBottomClothes] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [selectedTopClothes, setSelectedTopClothes] = useState([]);
+  const [selectedBottomClothes, setSelectedBottomClothes] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
-  const imagesBottoms = [
-    {
-      url: "https://example.com/image1.jpg",
-      caption: "Image 1",
-    },
-    {
-      url: "https://example.com/image2.jpg",
-      caption: "Image 2",
-    },
-    {
-      url: "https://example.com/image3.jpg",
-      caption: "Image 3",
-    },
-  ];
+  const styles = ["Casual", "Formal", "Cocktail", "Workout"];
 
-  const handleNextClick = (images) => {
-    if (currentIndex === images.length - 1) {
-      setCurrentIndex(0);
+  const fetchClothes = async () => {
+    const clothingCollection = collection(
+      db,
+      "clothes",
+      auth?.currentUser?.email,
+      "userclothes"
+    );
+    const q = query(clothingCollection);
+    const querySnapshot = await getDocs(q);
+    const topDocs = [];
+    const bottomDocs = [];
+    querySnapshot.forEach((doc) => {
+      if (doc.data()["category"] === "Top") {
+        topDocs.push({
+          ...doc.data(),
+        });
+      } else if (doc.data()["category"] === "Bottom") {
+        bottomDocs.push({
+          ...doc.data(),
+        });
+      }
+    });
+    setTopClothes(topDocs);
+    setBottomClothes(bottomDocs);
+  };
+
+  const fetchTags = async () => {
+    const tagsDocRef = doc(db, "tags", auth?.currentUser?.email);
+    getDoc(tagsDocRef).then((res) => {
+      setTags(res.data()["tags"]);
+    });
+  };
+
+  const handleTopNextClick = () => {
+    if (currentTopIndex === topClothes.length - 1) {
+      setCurrentTopIndex(0);
     } else {
-      setCurrentIndex(currentIndex + 1);
+      setCurrentTopIndex(currentTopIndex + 1);
     }
   };
 
-  const handlePrevClick = (images) => {
-    if (currentIndex === 0) {
-      setCurrentIndex(images.length - 1);
+  const handleTopPrevClick = () => {
+    if (currentTopIndex === 0) {
+      setCurrentTopIndex(topClothes.length - 1);
     } else {
-      setCurrentIndex(currentIndex - 1);
+      setCurrentTopIndex(currentTopIndex - 1);
     }
   };
+
+  const handleBottomPrevClick = () => {
+    if (currentTopIndex === 0) {
+      setCurrentBottomIndex(bottomClothes.length - 1);
+    } else {
+      setCurrentBottomIndex(currentTopIndex - 1);
+    }
+  };
+
+  const handleBottomNextClick = () => {
+    if (currentTopIndex === bottomClothes.length - 1) {
+      setCurrentBottomIndex(0);
+    } else {
+      setCurrentBottomIndex(currentTopIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      fetchClothes();
+      fetchTags();
+    }
+  }, [currentUser]);
 
   return (
     <div>
@@ -60,42 +110,97 @@ export default function MatchingPage() {
         </div>
         <div className="image-gallery-middle">
           <h4 className="image-gallery-middle-titles">STYLE</h4>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
+          {styles.map((style) => (
+            <p>{style}</p>
+          ))}
 
-          <h4 className="image-gallery-middle-titles">COLORS</h4>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-          <p>aaa</p>
-        
+          <h4 className="image-gallery-middle-titles">TAGS</h4>
+          {tags?.map((tag) => (
+            <p>{tag.name}</p>
+          ))}
         </div>
         <div className="image-gallery-right">
           <h5 className="image-gallery-secondTitle">TOP</h5>
           <div className="image-gallery-tops">
-            <button onClick={() => handlePrevClick(imagesTops)}></button>
+            <button
+              className="flex p-0 justify-end border-0 bg-transparent"
+              onClick={() => handleTopPrevClick()}
+            >
+              {" "}
+              <ArrowBackwardIosIcon
+                sx={{
+                  fontSize: "200px",
+                  color: "#ad7be9",
+                  ":hover": {
+                    color: "#ad7be9",
+                    opacity: "0.6",
+                  },
+                }}
+                color="inherit"
+              ></ArrowBackwardIosIcon>
+            </button>
             <img
-              src={imagesTops[currentIndex].url}
-              alt={imagesTops[currentIndex].caption}
+              src={topClothes[currentTopIndex]?.itemPic}
+              alt={topClothes[currentTopIndex]?.itemName}
             />
-            <button onClick={() => handleNextClick(imagesTops)}></button>
+            <button
+              className="flex p-0 justify-end border-0 bg-transparent"
+              onClick={() => handleTopNextClick()}
+            >
+              {" "}
+              <ArrowForwardIosIcon
+                sx={{
+                  fontSize: "200px",
+                  color: "#ad7be9",
+                  ":hover": {
+                    color: "#ad7be9",
+                    opacity: "0.6",
+                  },
+                }}
+                color="inherit"
+              ></ArrowForwardIosIcon>
+            </button>
           </div>
 
-          <div className="image-gallery-bottoms">
-            <button onClick={() => handlePrevClick(imagesBottoms)}></button>
+          <div className="image-gallery-bottoms ">
+            <button
+              onClick={() => handleBottomPrevClick()}
+              className="flex p-0 justify-end border-0 bg-transparent"
+            >
+              {" "}
+              <ArrowBackwardIosIcon
+                sx={{
+                  fontSize: "200px",
+                  color: "#ad7be9",
+                  ":hover": {
+                    color: "#ad7be9",
+                    opacity: "0.6",
+                  },
+                }}
+                color="inherit"
+              ></ArrowBackwardIosIcon>
+            </button>
             <img
-              src={imagesBottoms[currentIndex].url}
-              alt={imagesBottoms[currentIndex].caption}
+              src={bottomClothes[currentBottomIndex]?.itemPic}
+              alt={bottomClothes[currentBottomIndex]?.itemName}
             />
-            <button onClick={() => handleNextClick(imagesBottoms)}></button>
+            <button
+              onClick={() => handleBottomNextClick()}
+              className="flex p-0 justify-end border-0 bg-transparent"
+            >
+              {" "}
+              <ArrowForwardIosIcon
+                sx={{
+                  fontSize: "200px",
+                  color: "#ad7be9",
+                  ":hover": {
+                    color: "#ad7be9",
+                    opacity: "0.6",
+                  },
+                }}
+                color="inherit"
+              ></ArrowForwardIosIcon>
+            </button>
           </div>
           <h5 className="image-gallery-secondTitle">BOTTOM</h5>
         </div>
